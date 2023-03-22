e4fe926c13c230225a1bb856d0fc7e7544f99217
Adun Nantakaew <amphancm@gmail.com>
2023-03-22 09:46:12 +0700

    Update generate_val.py

--- generate_val.py
+++ generate_val.py
@@ -2,10 +2,10 @@ import os
 
 image_files = []
 i = 0
-os.chdir(os.path.join("data", "validation/snake"))
+os.chdir(os.path.join("data", "val"))
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".jpg"):
-        image_files.append("/content/pytorch-YOLOv4/data/validation/snake" + filename)
+        image_files.append("/content/pytorch-YOLOv4/data/val/" + filename)
         i+=1
 print('Total Val JPG file = ',i)  
 i = 0           
